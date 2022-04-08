d1fe579bd35953214841094b496a4af6a1eeef59
Jeff Knowlton <jeff.knowlton@ncr.com>
2022-04-08 14:27:37 -0500

    Made start-iberqs.ps1 a little more flexible

--- Start-IberQS.ps1
+++ Start-IberQS.ps1
@@ -1,1 +1,11 @@
-. C:\BootDrv\Aloha\BIN\iberqs.exe TERM 1
+$iberdir = @($env:IBERDIR, $env:LOCALDIR, 'C:\BOOTDRV\Aloha') | Select-Object -First 1
+if (!(Test-Path $iberdir)) {
+    throw "$iberdir does not exist"
+}
+
+$iberqs = "$iberdir\bin\iberqs.exe"
+if (!(Test-Path $aloha1)) {
+    throw "$aloha1 does not exist"
+}
+
+. $iberqs TERM 1
