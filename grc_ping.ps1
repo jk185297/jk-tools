a41929c6506af9078c43918ca7f02e11839c1254
Jeff Knowlton <jknowlton@OneCECO.com>
2023-07-17 14:22:05 -0500

    Different versions of grc_ping.ps1

--- grc_ping.ps1
+++ grc_ping.ps1
@@ -1,6 +1,30 @@
+$grcServers = @(
+	# "s-azp21.grc.local.unreachable",
+	"s-azp21.grc.local",
+	"s-azfile.grc.local",
+	"s-azsql.grc.local",
+	"s-azapp.grc.local"
+)
+
+$interval = 10
+$pingCount = 3
 $file = "~\grc_ping.txt"
+# overwrite file to ensure a clean file to start
+# out-file -filepath $file
+# Write-Host "Writing to '$file'"
 while (1) {
-    get-date | Out-File $file -Append
-    ping -n 5 s-azp21.grc.local | Out-File $file -Append
-    Start-Sleep 5
+    $results = @()
+    # get-date | tee-object -FilePath $file -Append
+    get-date
+    foreach ($grcServer in $grcServers) {
+        # ping -n $pingCount $grcServer | tee-object $file -Append
+        # test-netconnection $grcServer | tee-object $file -Append
+        $result = [PSCustomObject]@{
+            Server    = $grcServer
+            Reachable = Test-Connection -Quiet $grcServer
+        }
+        $results += $result
+    }
+    Write-Output $results | Format-Table 
+    Start-Sleep $interval
 }
